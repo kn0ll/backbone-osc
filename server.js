57a2814d81289b0e4fd67472a0425b9f19621c34
Nic Luciano <Nic@new-host-2.home>
2011-02-13 22:16:55 -0500

    i dont know how to commit things

--- server.js
+++ server.js
@@ -3,44 +3,9 @@ var Connect = require('connect'),
     Socket = require('socket.io-connect').socketIO,
     Fs = require('fs'),
     _ = require('./static/webroot/js/lib/underscore'),
-    osc = require('./lib/node-osc/lib/osc')
-    
-    var Client = function (port, host) {
-  this.port = port;
-  this.host = host // || '127.0.0.1';
-  this._sock = dgram.createSocket('udp4');
-}
-Client.prototype = {
-  send: function (msg) {
-    var binary = msg.toBinary();
-    var b = new buffer.Buffer(binary, 'binary');
-    console.log(msg);
-    //console.log(b, 0, b.length, this.port, this.host);
-    this._sock.send(b, 0, b.length, this.port, this.host);
-  },
-  sendSimple: function (address, data) {
-    var msg = new Message(address);
-    msg.append(data);
-    this.send(msg);
-  },
-}
-exports.Client = Client;
-
+    osc = require('./lib/node-osc/lib/osc');
     
 var server = Connect.createServer(
-    
-    Connect.staticProvider(__dirname + '/static'),
-    
-    Connect.router(function(app){
-        
-        app.get('/', function(req, res, next){
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            Fs.readFile('./views/index.html', 'utf8', function (err, html) {
-                res.end(_.template(html, req.params))
-            })
-        })
-        
-    }),
     
     Socket(function() { return server }, function (client, req, res) {
         
@@ -53,6 +18,19 @@ var server = Connect.createServer(
                  
             osc_client.sendSimple(args.join('/'), [val]);
            
+        })
+        
+    }),
+    
+    Connect.staticProvider(__dirname + '/static'),
+    
+    Connect.router(function(app){
+        
+        app.get('/', function(req, res, next){
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            Fs.readFile('./views/index.html', 'utf8', function (err, html) {
+                res.end(_.template(html, req.params))
+            })
         })
         
     })
