4a220de4f45abb1ca10869db473ae41c3620b96b
Nic Luciano <Nic@new-host-2.home>
2011-02-13 22:09:55 -0500

    fixing submodules for socket.io client and node-osc

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ var Connect = require('connect'),
     _ = require('./static/webroot/js/lib/underscore'),
     osc = require('./lib/node-osc/lib/osc')
     
+    var Client = function (port, host) {
+  this.port = port;
+  this.host = host // || '127.0.0.1';
+  this._sock = dgram.createSocket('udp4');
+}
+Client.prototype = {
+  send: function (msg) {
+    var binary = msg.toBinary();
+    var b = new buffer.Buffer(binary, 'binary');
+    console.log(msg);
+    //console.log(b, 0, b.length, this.port, this.host);
+    this._sock.send(b, 0, b.length, this.port, this.host);
+  },
+  sendSimple: function (address, data) {
+    var msg = new Message(address);
+    msg.append(data);
+    this.send(msg);
+  },
+}
+exports.Client = Client;
+
+    
 var server = Connect.createServer(
     
     Connect.staticProvider(__dirname + '/static'),
